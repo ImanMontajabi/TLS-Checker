9f0c638835c943922e0dbd06554fcfd824aea2f5
Iman <iman.montajabi@gmail.com>
2023-05-14 13:10:25 -0700

    progress spinner

--- tls-checker.py
+++ tls-checker.py
@@ -6,9 +6,11 @@ import concurrent.futures
 import dns.resolver
 import requests
 import random
+from progress.spinner import PixelSpinner
+
 
 web_addrs = [] # all urls from csv
-take_file_name = input('Which file? [irani or all]:')
+take_file_name = input('- Which file? [irani or all]:')
 file_name = take_file_name if (take_file_name == 'irani') or (take_file_name == 'all') else 'all'
 with open(f'./{file_name}.csv') as urls:
     csv_reader = csv.reader(urls)
@@ -19,20 +21,19 @@ random.shuffle(web_addrs)
 # take length of csv chunk
 input_urls = [] # list for threads
 try:
-    how_many = int(input(f'How many url of "{file_name}.csv" do you want to check? [1-{len_webaddr}]:'))
+    how_many = int(input(f'- How many url of "{file_name}.csv" do you want to check? [1-{len_webaddr}]:'))
 except ValueError:
     how_many = len_webaddr
 
 how_many = max(how_many, 1)
 how_many = min(how_many, len_webaddr)
 # make input of threads
-length = 20 if how_many >= 20 else 1
+length = 10 if how_many >= 10 else 1
 for i in range(0, how_many, length):
     input_urls.append(web_addrs[i:min(i+length, how_many)])
 # set iso code
-print('Hint: https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes')
-take_iso_name = input('preferred country? [Germany = DE, Netherland = NL, ...]:')
-print('======================', end='')
+print('+ Hint: https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes')
+take_iso_name = input('- preferred country? [Germany = DE, Netherland = NL, ...]:')
 
 
 def get_info(web_addrs: list) -> dict:
@@ -56,29 +57,40 @@ def get_info(web_addrs: list) -> dict:
             alpn = conn.selected_alpn_protocol()
             iso_code = response['country']['iso_code']
             if take_iso_name != '':
-                if (cipher[1] == 'TLSv1.3') and ((alpn == 'h2') or (alpn == 'h3')) and (take_iso_name == iso_code):
-                    country = response['country']['names']['en']
-                    city = response['city']['names']['en']
-                    isp = response['traits']['isp']
-                    issuer = conn.getpeercert()['issuer'][1][0][1]
-                    print(f'\naddress = {web_addr}\nalpn = {conn.selected_alpn_protocol()}\nissuer = {issuer}\ncipher = {cipher[0]}\nTLS = {cipher[1]}\nkey_length = {cipher[2]}\ncountry = {country}\niso_code = {iso_code}\ncity = {city}\nisp = {isp}\n---------------------', end='')
-                    result[web_addr] = [issuer, alpn, cipher[0], cipher[1], cipher[2], country, iso_code, city, isp]
+                spinner = PixelSpinner('----------------------- Searching ')
+                state = 'RUNNING'
+                while state == 'RUNNING':
+                    spinner.next()
+                    if (cipher[1] == 'TLSv1.3') and ((alpn == 'h2') or (alpn == 'h3')) and (take_iso_name == iso_code):
+                        country = response['country']['names']['en']
+                        city = response['city']['names']['en']
+                        isp = response['traits']['isp']
+                        issuer = conn.getpeercert()['issuer'][1][0][1]
+                        print(f'\naddress = {web_addr}\nalpn = {conn.selected_alpn_protocol()}\nissuer = {issuer}\ncipher = {cipher[0]}\nTLS = {cipher[1]}\nkey_length = {cipher[2]}\ncountry = {country}\niso_code = {iso_code}\ncity = {city}\nisp = {isp}\n', end='')
+                        result[web_addr] = [issuer, alpn, cipher[0], cipher[1], cipher[2], country, iso_code, city, isp]
+                        state = 'FINISHED'
                 else:
                     continue
             else:
-                if (cipher[1] == 'TLSv1.3') and ((alpn == 'h2') or (alpn == 'h3')):
-                    country = response['country']['names']['en']
-                    city = response['city']['names']['en']
-                    isp = response['traits']['isp']
-                    issuer = conn.getpeercert()['issuer'][1][0][1]
-                    print(f'\naddress = {web_addr}\nalpn = {conn.selected_alpn_protocol()}\nissuer = {issuer}\ncipher = {cipher[0]}\nTLS = {cipher[1]}\nkey_length = {cipher[2]}\ncountry = {country}\niso_code = {iso_code}\ncity = {city}\nisp = {isp}\n---------------------', end='')
-                    result[web_addr] = [issuer, alpn, cipher[0], cipher[1], cipher[2], country, iso_code, city, isp]
+                spinner = PixelSpinner('----------------------- Searching ')
+                state = 'RUNNING'
+                while state == 'RUNNING':
+                    spinner.next()
+                    if (cipher[1] == 'TLSv1.3') and ((alpn == 'h2') or (alpn == 'h3')):
+                        country = response['country']['names']['en']
+                        city = response['city']['names']['en']
+                        isp = response['traits']['isp']
+                        issuer = conn.getpeercert()['issuer'][1][0][1]
+                        print(f'\naddress = {web_addr}\nalpn = {conn.selected_alpn_protocol()}\nissuer = {issuer}\ncipher = {cipher[0]}\nTLS = {cipher[1]}\nkey_length = {cipher[2]}\ncountry = {country}\niso_code = {iso_code}\ncity = {city}\nisp = {isp}\n', end='')
+                        result[web_addr] = [issuer, alpn, cipher[0], cipher[1], cipher[2], country, iso_code, city, isp]
+                        state = 'FINISHED'
                 else:
                     continue
     return result
-        
+
+   
 outlist = []
-with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executer:
+with concurrent.futures.ThreadPoolExecutor(max_workers=30) as executer:
     tasks = [executer.submit(get_info, url_group) for url_group in input_urls]
     for task in concurrent.futures.as_completed(tasks):
         result = task.result()
