8b80cc8e823cce27d27db5149c8f5426f362b517
Iman <iman.montajabi@gmail.com>
2023-05-14 14:53:34 -0700

    stable

--- tls-checker.py
+++ tls-checker.py
@@ -28,11 +28,11 @@ except ValueError:
 how_many = max(how_many, 1)
 how_many = min(how_many, len_webaddr)
 # make input of threads
-length = 10 if how_many >= 10 else 1
+length = 20 if how_many >= 20 else 1
 for i in range(0, how_many, length):
     input_urls.append(web_addrs[i:min(i+length, how_many)])
 # set iso code
-print('+ Hint: https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes')
+print('* Hint: https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes')
 take_iso_name = input('- preferred country? [Germany = DE, Netherland = NL, ...]:')
 
 
@@ -57,33 +57,23 @@ def get_info(web_addrs: list) -> dict:
             alpn = conn.selected_alpn_protocol()
             iso_code = response['country']['iso_code']
             if take_iso_name != '':
-                spinner = PixelSpinner('----------------------- Searching ')
-                state = 'RUNNING'
-                while state == 'RUNNING':
-                    spinner.next()
-                    if (cipher[1] == 'TLSv1.3') and ((alpn == 'h2') or (alpn == 'h3')) and (take_iso_name == iso_code):
-                        country = response['country']['names']['en']
-                        city = response['city']['names']['en']
-                        isp = response['traits']['isp']
-                        issuer = conn.getpeercert()['issuer'][1][0][1]
-                        print(f'\naddress = {web_addr}\nalpn = {conn.selected_alpn_protocol()}\nissuer = {issuer}\ncipher = {cipher[0]}\nTLS = {cipher[1]}\nkey_length = {cipher[2]}\ncountry = {country}\niso_code = {iso_code}\ncity = {city}\nisp = {isp}\n', end='')
-                        result[web_addr] = [issuer, alpn, cipher[0], cipher[1], cipher[2], country, iso_code, city, isp]
-                        state = 'FINISHED'
+                if (cipher[1] == 'TLSv1.3') and ((alpn == 'h2') or (alpn == 'h3')) and (take_iso_name == iso_code):
+                    country = response['country']['names']['en']
+                    city = response['city']['names']['en']
+                    isp = response['traits']['isp']
+                    issuer = conn.getpeercert()['issuer'][1][0][1]
+                    print(f'\naddress = {web_addr}\nalpn = {conn.selected_alpn_protocol()}\nissuer = {issuer}\ncipher = {cipher[0]}\nTLS = {cipher[1]}\nkey_length = {cipher[2]}\ncountry = {country}\niso_code = {iso_code}\ncity = {city}\nisp = {isp}\n', end='')
+                    result[web_addr] = [issuer, alpn, cipher[0], cipher[1], cipher[2], country, iso_code, city, isp]
                 else:
                     continue
             else:
-                spinner = PixelSpinner('----------------------- Searching ')
-                state = 'RUNNING'
-                while state == 'RUNNING':
-                    spinner.next()
-                    if (cipher[1] == 'TLSv1.3') and ((alpn == 'h2') or (alpn == 'h3')):
-                        country = response['country']['names']['en']
-                        city = response['city']['names']['en']
-                        isp = response['traits']['isp']
-                        issuer = conn.getpeercert()['issuer'][1][0][1]
-                        print(f'\naddress = {web_addr}\nalpn = {conn.selected_alpn_protocol()}\nissuer = {issuer}\ncipher = {cipher[0]}\nTLS = {cipher[1]}\nkey_length = {cipher[2]}\ncountry = {country}\niso_code = {iso_code}\ncity = {city}\nisp = {isp}\n', end='')
-                        result[web_addr] = [issuer, alpn, cipher[0], cipher[1], cipher[2], country, iso_code, city, isp]
-                        state = 'FINISHED'
+                if (cipher[1] == 'TLSv1.3') and ((alpn == 'h2') or (alpn == 'h3')):
+                    country = response['country']['names']['en']
+                    city = response['city']['names']['en']
+                    isp = response['traits']['isp']
+                    issuer = conn.getpeercert()['issuer'][1][0][1]
+                    print(f'\naddress = {web_addr}\nalpn = {conn.selected_alpn_protocol()}\nissuer = {issuer}\ncipher = {cipher[0]}\nTLS = {cipher[1]}\nkey_length = {cipher[2]}\ncountry = {country}\niso_code = {iso_code}\ncity = {city}\nisp = {isp}\n', end='')
+                    result[web_addr] = [issuer, alpn, cipher[0], cipher[1], cipher[2], country, iso_code, city, isp]
                 else:
                     continue
     return result
